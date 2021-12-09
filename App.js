1d264d39bdbb17977d09390ac35241f4f8d5d166
intissar99 <90939476+intissar99@users.noreply.github.com>
2021-12-09 07:48:58 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -3,35 +3,45 @@ import Connexion from './Components/Connexion';
 import Inscription from './Components/Inscription';
 import Home from './Components/Home';
 import {makeStyles} from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import Icones from './Components/Icones';
 const useStyles= makeStyles((theme)=>({
   root: {
       minHeight: '100vh',
-      backgroundImage: `url(${process.env.PUBLIC_URL + '/Assets/Image.jpg'})`,
+      backgroundImage: `url(${process.env.PUBLIC_URL + '/Assets/image.jpg'})`,
       backgroundRepeat: 'no-repeat',
       backgroundSize: 'cover',
+      
     }
   }))
 
 function App() {
-  const Style=useStyles();
+  const style=useStyles();
   return (
-    <div className={Style.root}>
+    <div className={style.root}>
+      <span >
+       <CssBaseline />
+       <Home/>
+       <Icones/>
+      </span>
     <Router>
       <Routes>
+     
         <Route path='/Connexion' element={<Connexion/>}/>
         <Route path='/Inscription' element={<Inscription/>}/>
         <Route path='/Home' element={<Home/>}/>
+        <Route path='/Icones' element={<Icones/>}/>
       </Routes>
        
      </Router>
    
    
    
- </div>  
-    
+ </div> 
+ 
+   
   );
 }
-   
 
 export default App;
